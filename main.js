ab335faf762c00c0ec526646d0a8a0605ad7e1b2
Daniel Andrade <67202123+andrsx@users.noreply.github.com>
2023-05-17 10:26:02 -0300

    Arquivos iniciais

---
+++ main.js
@@ -0,0 +1,34 @@
+function adicionarClassesAtivas() {
+  const links = document.querySelectorAll('.sobre__coluna-2-tabs-links')
+  // Loop através de cada link
+  links.forEach(link => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault()
+      const tag = event.target
+      console.log(tag.id)
+      // Remova a classe ativa de todos os links
+      links.forEach(link => {
+        link.classList.remove('link-ativo')
+        removeConteudo()
+      })
+      // Adicione a classe ativa no link clicado
+      this.classList.add('link-ativo')
+      adicionaConteudo(tag.id)
+    })
+  })
+}
+
+adicionarClassesAtivas()
+
+function removeConteudo() {
+  const conteudo = document.querySelectorAll('.sobre__coluna-2-conteudo')
+  conteudo.forEach(Conteudo => {
+    Conteudo.classList.remove('tab-ativo')
+  })
+}
+
+function adicionaConteudo(id) {
+  const conteudo = document.getElementById(`${id}-conteudo`)
+  conteudo.classList.add('tab-ativo')
+  //console.log(conteudo)
+}
